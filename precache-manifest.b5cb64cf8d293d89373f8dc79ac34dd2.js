b64e909c89a5773c45abf013f0d5bea4c269045e
Chris Martyr <chrismartyr90@gmail.com>
2020-01-15 15:11:28 -0800

    Updates

---
+++ precache-manifest.b5cb64cf8d293d89373f8dc79ac34dd2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ef3d902175412ffe757a68c4e5f8fb",
+    "url": "/remote-job-board-app/index.html"
+  },
+  {
+    "revision": "e460705eda4a913182fb",
+    "url": "/remote-job-board-app/static/css/2.e95dff61.chunk.css"
+  },
+  {
+    "revision": "bc84156d85477d73e5ad",
+    "url": "/remote-job-board-app/static/css/main.2eb46174.chunk.css"
+  },
+  {
+    "revision": "e460705eda4a913182fb",
+    "url": "/remote-job-board-app/static/js/2.8fccfa1d.chunk.js"
+  },
+  {
+    "revision": "4ec6b21b6d70e99d4160938106e9ca55",
+    "url": "/remote-job-board-app/static/js/2.8fccfa1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc84156d85477d73e5ad",
+    "url": "/remote-job-board-app/static/js/main.ef3f9328.chunk.js"
+  },
+  {
+    "revision": "6c75aaa7e95f3f35e251",
+    "url": "/remote-job-board-app/static/js/runtime-main.588d3334.js"
+  },
+  {
+    "revision": "4a22bd7c0b3edca3e592b987d48be344",
+    "url": "/remote-job-board-app/static/media/remote-banner.4a22bd7c.jpg"
+  }
+]);
